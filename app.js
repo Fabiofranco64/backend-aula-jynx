baa90adf7e4284156ab5518ffdd022bd9b2792fb
Fabiofranco64 <f.franco64@gmail.com>
2026-02-25 21:53:39 -0300

    iniciakização do server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("teste");
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+  return "Bem vindo a aula de backend"
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
